20b022dd073b921799d59a9d2769d7e1c51f5c0e
EngineerMoore <alexiashalise@gmail.com>
2025-06-22 09:10:49 -0500

    created a navbar and stored login information in context

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,30 @@
-import './App.css'
 import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
 import { Container } from 'reactstrap'
 import { Routes, Route } from 'react-router-dom'
 import Register from './pages/Register'
 import EventsPage from './pages/EventsPage'
+import TopNav from './components/TopNav'
+import { ContextWrapper } from './user-context'
+import './App.css'
 
 function App() {
   return (
     <>
-      <Container>
-        <h1>Sport's App</h1>
-        <div className='content'>
-          <Routes>
-            <Route path='/' element={<Dashboard />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/register' element={<Register />} />
-            <Route path='/events' element={<EventsPage />} />
-          </Routes>
-        </div>
-      </Container>
+      <ContextWrapper>
+        <Container>
+          <h1>Sport's App</h1>
+          <div className='content'>
+            <TopNav />
+            <Routes>
+              <Route path='/' element={<Dashboard />} />
+              <Route path='/login' element={<Login />} />
+              <Route path='/register' element={<Register />} />
+              <Route path='/events' element={<EventsPage />} />
+            </Routes>
+          </div>
+        </Container>
+      </ContextWrapper>
     </>
   )
 }
