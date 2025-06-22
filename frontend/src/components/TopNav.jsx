84fd9a78e2f84dd2c14c025c494d4e308635bb05
EngineerMoore <alexiashalise@gmail.com>
2025-06-22 09:15:34 -0500

    removed console log

--- frontend/src/components/TopNav.jsx
+++ frontend/src/components/TopNav.jsx
@@ -25,7 +25,6 @@ const TopNav = () => {
 
   return isLoggedIn ?
     <div>
-      {console.log(isLoggedIn)}
       <Navbar color="faded" light>
         <NavbarToggler onClick={toggleNavbar}/>
         <Link to='/login' onClick={logoutHandler}>Logout</Link>
